8c79a4230ee2254fba868c4c8cd3bf219d643336
trevthedev254 <trevetech@gmail.com>
2022-04-12 14:53:19 +0000

    lowercase alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print lowercase alphabet 10 times
+ *
+ * Return: None
+ */
+void print_alphabet_x10(void)
+{
+	char a;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (a = 'a'; a <= 'z'; a++)
+		{
+			_putchar(a);
+		}
+		_putchar('\n');
+	}
+
+}
