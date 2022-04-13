5d8a5ad1374e9f6f6fd2149611a8269a6ea1a482
trevthedev254 <trevetech@gmail.com>
2022-04-13 16:11:39 +0000

    fibonacci first 50

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,19 +7,20 @@
  */
 int main(void)
 {
-	int i = 1;
-       	int j = 2;
-       	int next = 0;
+	int i;
+	long int next[50];
 
-	printf("%d, %d, ", i , j);
-	next = i + j;
+	next[0] = 1;
+	next[1] = 2;
+	printf("%ld, %ld, ", next[0], next[1]);
 
-	while (next <= 50)
+	for (i = 2; i < 50; i++)
 	{
-		printf("%d, ", next);
-		i = j;
-		j = next;
-		next = i + j;
+		next[i] = next[i - 1] + next[i - 2];
+		if (i == 49)
+			printf("%ld\n", next[i]);
+		else
+			printf("%ld, ", next[i]);
 	}
 
 	return (0);
