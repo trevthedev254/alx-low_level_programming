b5c9e4afbad427cd60463840ce9218a5fc2702d1
trevthedev254 <trevetech@gmail.com>
2022-04-13 17:04:27 +0000

    sum of even numbers in fibonacci sequence

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main -  sum of even fibonnacci numbers
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i = 1, j = 2, next = 0;
+
+	while (next <= 4000000 && next % 2 == 0)
+	{
+		i = j;
+		j = next;
+		next = i + j;
+	}
+	printf("%d\n", next);
+
+	return (0);
+}
