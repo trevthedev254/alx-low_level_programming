be04a3f98a28cecebce0228ca107891fc06e653e
trevthedev254 <trevetech@gmail.com>
2022-04-14 13:51:53 +0000

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,11 +15,11 @@ int main(void)
 		{
 			printf("%d", i);
 		}
-		else if ((i % 3) == 0)
+		else if ((i % 3) == 0 && (i % 5) != 0)
 		{
 			printf(" Fizz");
 		}
-		else if ((i % 5) == 0)
+		else if ((i % 5) == 0 && (i % 3) != 0)
 		{
 			printf(" Buzz");
 		}
