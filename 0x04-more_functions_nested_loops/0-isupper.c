41a4b226c55bf6bf5bbd117ccfda2262b2e4678b
trevthedev254 <trevetech@gmail.com>
2022-04-14 10:11:56 +0000

    uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: character input
+ * Return: 1 if uppercase 0 if lowecase
+ */
+
+int _isupper(int c)
+{
+	char upper;
+	int alpha;
+
+	for (upper = 'A'; upper <= 'Z'; upper++)
+	{
+		if (upper == c)
+		{
+			alpha = 1;
+			_putchar(upper + '0');
+			_putchar(':');
+			_putchar(' ');
+			_putchar(alpha + '0');
+		}
+		else
+		{
+			alpha = 0;
+			_putchar(upper + '0');
+                        _putchar(':');
+                        _putchar(' ');
+                        _putchar(alpha + '0');
+		}
+	}
+
+	return (alpha);
+}
