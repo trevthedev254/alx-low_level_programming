6b0a910ed1f0b4b78292109adcceec16faf1d183
trevthedev254 <trevetech@gmail.com>
2022-04-27 16:14:40 +0000

    chess and diagonal

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - prints the sum of the two diagonals
+ * of a square matrix of integers
+ * @a: square matrix of which we print the sum of diagonals
+ * @size: size of the matrix
+ */
+void print_diagsums(int *a, int size)
+{
+	int i = 0;
+	int max = size * size;
+	int sum1 = 0;
+	int sum2 = 0;
+
+	for (i = 0; i < max; i += size + 1)
+		sum1 += a[i];
+
+	for (i = size - 1; i < max - 1; i += size - 1)
+		sum2 += a[i];
+
+	printf("%d, %d\n", sum1, sum2);
+}
