fbe84f770276dd7a06ba1fe05a9932be4447e08a
trevthedev254 <trevetech@gmail.com>
2022-05-06 14:13:47 +0000

    malloc

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -9,43 +9,35 @@
  */
 char **strtow(char *str)
 {
-	char **array;
-	int i = 0, j, m, k = 0, len = 0, count = 0;
+	char **d;
+	int i;
+	int j = 0;
+	int con = 0;
 
-	if (str == NULL || *str == '\0')
+	if (str == NULL)
 		return (NULL);
-	for (; str[i]; i++)
+
+	for (i = 0 ; str[i] != '\0' ; i++)
 	{
-		if ((str[i] != ' ' || *str != '\t') &&
-				((str[i + 1] == ' ' || str[i + 1] == '\t') || str[i + 1] == '\n'))
-			count++;
+		if (str[i] != 32)
+			con++;
 	}
-	if (count == 0)
+
+	d = malloc(sizeof(char) * con);
+
+	if (d == NULL)
 		return (NULL);
-	array = malloc(sizeof(char *) * (count + 1));
-	if (array == NULL)
-		return (NULL);
-	for (i = 0; str[i] != '\0' && k < count; i++)
+
+	for (i = 0 ; str[i] != '\0' ; i++)
 	{
-		if (str[i] != ' ' || str[i] != '\t')
+		if (str[i] != 32)
 		{
-			len = 0;
-			j = i;
-			while ((str[j] != ' ' || str[j] != '\t') && str[j] != '\0')
-				j++, len++;
-			array[k] = malloc((len + 1) * sizeof(char));
-			if (array[k] == NULL)
-			{
-				for (k = k - 1; k >= 0; k++)
-					free(array[k]);
-				free(array);
-				return (NULL);
-			}
-			for (m = 0; m < len; m++, i++)
-				array[k][m] = str[i];
-			array[k++][m] = '\0';
+			*d[j] = str[i];
+			j++;
+		}
+		else
+		{
 		}
 	}
-	array[k] = NULL;
-	return (array);
+	return (d);
 }
