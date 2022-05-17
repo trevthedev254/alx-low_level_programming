b6e93259c43b20216e0d0a1ea8efc1076613f194
trevthedev254 <trevetech@gmail.com>
2022-05-17 14:26:04 +0000

    correction

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -13,6 +13,9 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i;
 	va_list valist;
 
+	if (n == 0)
+		return (0);
+
 	va_start(valist, n);
 	for (i = 0; i < n; i++)
 		sum += va_arg(valist, int);
