b8de06dd54acf178f7ea139a903e139d27b5b579
trevthedev254 <trevetech@gmail.com>
2022-05-25 14:11:52 +0000

    no of elements

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,17 +9,17 @@
  */
 size_t print_list(const list_t *h)
 {
-	const list_t *tmp;
-	unsigned int i;
+	size_t s = 0;
 
-	tmp = h;
-	if (!h->str)
-		printf("[0] (nil)\n");
-	else
-		for (i = 0; tmp; i++)
-		{
-			printf("[%u] %s\n", tmp->len, tmp->str);
-			tmp = tmp->next;
-		}
-	return (i);
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		s++;
+	}
+
+	return (s);
 }
